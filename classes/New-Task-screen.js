d4a1557feb0989ebc1c7670f628ac545ee5648e3
markomilenkovic546 <markomilenkovic546@gmail.com>
2023-08-23 17:46:33 +0200

    create part of the task creation tests

--- classes/New-Task-screen.js
+++ classes/New-Task-screen.js
@@ -1,12 +1,148 @@
 class NewTaskScreen {
-    // Elements
-  
-
-  
-    //actions
- 
-  
-    
+  // Elements
+  async screenTitle(screenTitle) {
+    const selector = `new UiSelector().className("android.widget.TextView").text("${screenTitle}")`;
+    await $(`android=${selector}`).waitForExist();
+    const title = $(`android=${selector}`);
+    return title;
   }
-  module.exports = new NewTaskScreen();
-  
+
+  async taskDescriptionInputField() {
+    const selector = `new UiSelector().className("android.widget.EditText").text("Enter Task Here")`;
+    await $(`android=${selector}`).waitForExist();
+    const enterTaskField = $(`android=${selector}`);
+    return enterTaskField;
+  }
+
+  async dueDateInputField() {
+    const selector = `new UiSelector().className("android.widget.EditText").text("Date not set")`;
+    await $(`android=${selector}`).waitForExist();
+    const dueDateField = $(`android=${selector}`);
+    return dueDateField;
+  }
+
+  async okButton() {
+    const selector = `new UiSelector().className("android.widget.Button").text("OK")`;
+    await $(`android=${selector}`).waitForExist();
+    const okBtn = $(`android=${selector}`);
+    return okBtn;
+  }
+
+  async dueDateButton(dueDate) {
+    const saveTaskButton = await $(`~${dueDate}`);
+    return saveTaskButton;
+  }
+
+  get saveTaskButton() {
+    const saveTaskButton = $("~Save Task");
+    return saveTaskButton;
+  }
+
+  get addTolistDropDown() {
+    const dropdown = $(`android=new UiSelector().resourceId("com.splendapps.splendo:id/spinnerLists")`);
+    return dropdown;
+  }
+
+  async listOptionFromListDDM(listName) {
+    const selector = `new UiSelector().className("android.widget.TextView").text("${listName}")`;
+    await $(`android=${selector}`).waitForExist();
+
+    const list = $(`android=${selector}`);
+    return list;
+  }
+
+  get addListButton() {
+    const addListButton = $("~Add");
+    return addListButton;
+  }
+
+  /////////actions
+  async typeTaskDescription(taskText) {
+    const task = await this.taskDescriptionInputField();
+    await task.setValue(taskText);
+  }
+
+  async clickOnSaveTaskButton() {
+    await this.saveTaskButton.click();
+  }
+
+  async openDueDateCalendar() {
+    const dueDateField = await this.dueDateInputField();
+    await dueDateField.click();
+  }
+
+  async selectDueDate() {
+    function formatDate(date) {
+      const day = date.getDate();
+      const month = date.toLocaleString("default", { month: "long" });
+      const year = date.getFullYear();
+      return `${day} ${month} ${year}`;
+    }
+
+    const today = new Date();
+    const formattedDate = formatDate(today);
+    console.log(formattedDate);
+
+    const dueDateBtn = await this.dueDateButton(formattedDate);
+    await dueDateBtn.click();
+  }
+
+  async clickOkButton() {
+    const okBtn = await this.okButton();
+    await okBtn.click();
+  }
+
+  async openAddTolistDropDown() {
+    const dropdown = await this.addTolistDropDown;
+    await dropdown.click();
+  }
+
+  async selectListOptionFromDDM(listName) {
+    const list = await this.listOptionFromListDDM(listName);
+    await list.click();
+  }
+
+  async clickOnAddListButton() {
+    await this.addListButton.click();
+  }
+
+  async swipeToTheBottom() {
+    const { height } = await driver.getWindowSize();
+    const anchorPercentage = 50;
+    const startPointPercentage = 55;
+    const endPointPercentage = 10;
+
+    const anchor = (height * anchorPercentage) / 100;
+    const startPoint = (height * startPointPercentage) / 100;
+    const endPoint = (height * endPointPercentage) / 100;
+
+    // Press on start point, wait 1s, moveTo endPoint , release
+    await driver.touchPerform([
+      {
+        action: "press",
+        options: {
+          x: anchor,
+          y: startPoint,
+        },
+      },
+      {
+        action: "wait",
+        options: {
+          ms: 1000,
+        },
+      },
+      {
+        action: "moveTo",
+        options: {
+          x: anchor,
+          y: endPoint,
+        },
+      },
+      {
+        action: "release",
+        options: {},
+      },
+    ]);
+  }
+}
+module.exports = new NewTaskScreen();
--- e2e-specs/task-management.js
+++ e2e-specs/task-management.js
@@ -1,47 +1,130 @@
 const HomeScreen = require("../classes/home-screen");
-const lists = require("../fixtures/lists.json");
+const NewTaskScreen = require("../classes/new-task-screen");
+const ListScreen = require("../classes/list-screen");
+const lists = require("../fixtures/todo-lists.json");
+
 
 describe("Task Management", () => {
-
   beforeEach(async () => {
-    await HomeScreen.openListsDropDown();
-    const listName = "All Lists"
-    // Select "Default" option from the DDM
-    await HomeScreen.selectListOptionFromDDM(listName)
+    // Launch the app before each test
+    await driver.launchApp();
   });
+
   it("User can create a Quick task", async () => {
     // Type Quick Task description
-    await HomeScreen.typeQuickTask(lists.Default.quickTask);
+    await HomeScreen.typeQuickTask(lists[0].Default[0]);
     // Click on the "Add task" button
     await HomeScreen.clickOnAddQuickTaskButton();
     // Get created quick task and verify that is created
-    const task = await HomeScreen.taskFromTheList(lists.Default.quickTask)
-    const taskDescription = await task.getText()
-    await expect(taskDescription).toEqual(lists.Default.quickTask);
+    const task = await HomeScreen.taskFromTheList(lists[0].Default[0]);
+    const taskDescription = await task.getText();
+    await expect(taskDescription).toEqual(lists[0].Default[0]);
   });
 
-  it("Quick task is placed in the Default list once the user create it", async () => {
+  it("Quick task is saved in the Default list ", async () => {
     // Type Quick Task description
-    await HomeScreen.typeQuickTask(lists.Default.quickTask2);
+    await HomeScreen.typeQuickTask(lists[0].Default[1]);
     // Click on the "Add task" button
     await HomeScreen.clickOnAddQuickTaskButton();
-    // Open lists drop down 
+    // Open lists drop down
     await HomeScreen.openListsDropDown();
-    const lname = "Default"
+    const lname = "Default";
     // Select "Default" option from the DDM
-    await HomeScreen.selectListOptionFromDDM(lname)
+    await HomeScreen.selectListOptionFromDDM(lname);
     // Get created task and verify that is placed in the "Default" list
-    const task = await HomeScreen.taskFromTheList(lists.Default.quickTask2)
-    const taskDescription = await task.getText()
-    await expect(taskDescription).toEqual(lists.Default.quickTask2);
+    const task = await ListScreen.taskFromTheList(lists[0].Default[1]);
+    const taskDescription = await task.getText();
+    await expect(taskDescription).toEqual(lists[0].Default[1]);
   });
 
   it("User can open the 'New Task' screen", async () => {
-    await browser.pause(6000);
+    // Click on the "Add task" (+) button
     await HomeScreen.clickOnAddTaskButton();
-    await browser.pause(6000);
-    const screenTitle = await HomeScreen.screenTitle("New Task")
+    const screenTitle = await NewTaskScreen.screenTitle("New Task");
     await screenTitle.waitForExist();
+    // Verify that "New Task" screen is displayed
     expect(await screenTitle.isExisting()).toBe(true);
+  });
+
+  it.skip("User can create a task from the 'New Task' screen", async () => {
+    // Click on the "Add task" (+) button
+    await HomeScreen.clickOnAddTaskButton();
+    await NewTaskScreen.typeTaskDescription(lists[0].Default[2]);
+    await NewTaskScreen.openDueDateCalendar();
+    await NewTaskScreen.selectDueDate();
+    await NewTaskScreen.clickOkButton();
+    await NewTaskScreen.clickOnSaveTaskButton();
+    const task = await HomeScreen.taskFromTheList(lists[0].Default[2]);
+    const taskDescription = await task.getText();
+    await expect(taskDescription).toEqual(lists[0].Default[2]);
+  });
+
+  it("Create task with title only (Minimum data required)", async () => {
+    await HomeScreen.clickOnAddTaskButton();
+    await NewTaskScreen.typeTaskDescription(lists[0].Default[2]);
+    await NewTaskScreen.clickOnSaveTaskButton();
+    await HomeScreen.openListsDropDown();
+    await HomeScreen.selectListOptionFromDDM("Default");
+    const task = await HomeScreen.taskFromTheList(lists[0].Default[2]);
+    const taskDescription = await task.getText();
+    await expect(taskDescription).toEqual(lists[0].Default[2]);
+  });
+
+  it("Create task with due time", async () => {
+    await HomeScreen.clickOnAddTaskButton();
+    await NewTaskScreen.typeTaskDescription(lists[0].Default[2]);
+    await NewTaskScreen.openDueDateCalendar();
+    await NewTaskScreen.selectDueDate();
+    await NewTaskScreen.clickOkButton();
+    await NewTaskScreen.clickOnSaveTaskButton();
+    await HomeScreen.openListsDropDown();
+    // Select "Default" option from the DDM
+    await HomeScreen.selectListOptionFromDDM("Default");
+    const task = await HomeScreen.taskFromTheList(lists[0].Default[2]);
+    const taskDescription = await task.getText();
+    // Verify that task is created and is it's saved in the correct default list
+    await expect(taskDescription).toEqual(lists[0].Default[2]);
+    const taskDueDateElement = await HomeScreen.taskDueDate("Today");
+    const dueDateText = await taskDueDateElement.getText();
+    //Created task has correctly set due date
+    await expect(dueDateText).toEqual("Today");
+  });
+
+  it("Create task with specific list", async () => {
+    await HomeScreen.clickOnAddTaskButton();
+    await NewTaskScreen.typeTaskDescription(lists[0].Default[3]);
+    await driver.hideKeyboard();
+    await NewTaskScreen.openAddTolistDropDown();
+    await NewTaskScreen.selectListOptionFromDDM("Shopping");
+    await NewTaskScreen.clickOnSaveTaskButton();
+    await HomeScreen.openListsDropDown();
+    await HomeScreen.selectListOptionFromDDM("Shopping");
+    // Verify that task is saved in the correct list
+    const task = await ListScreen.taskFromTheList(lists[0].Default[3]);
+  });
+
+  
+  it.only("Create task with all options", async () => {
+    await HomeScreen.clickOnAddTaskButton();
+    await NewTaskScreen.typeTaskDescription(lists[0].Default[3]);
+    await NewTaskScreen.openDueDateCalendar();
+    await NewTaskScreen.selectDueDate();
+    await NewTaskScreen.clickOkButton();
+    await driver.hideKeyboard();
+    await NewTaskScreen.swipeToTheBottom()
+    await NewTaskScreen.openAddTolistDropDown();
+    await NewTaskScreen.selectListOptionFromDDM("Shopping");
+    
+    await NewTaskScreen.clickOnSaveTaskButton();
+    await HomeScreen.openListsDropDown();
+    await HomeScreen.selectListOptionFromDDM("Shopping");
+    // Verify that task is saved in the correct list
+    const task = await ListScreen.taskFromTheList(lists[0].Default[3]);
+    const taskDueDateElement = await ListScreen.taskDueDate("Today");
+    const dueDateText = await taskDueDateElement.getText();
+    //Created task has correctly set due date
+    await expect(dueDateText).toEqual("Today");
+  
+    await browser.pause(6000);
   });
 });
